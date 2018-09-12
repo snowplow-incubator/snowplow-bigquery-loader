0f65db8438aece7606ed8c673e586606cee5b969
Anton Parkhomenko <mailbox@chuwy.me>
2018-09-12 18:41:44 +0300

    Actually add forwarder

---
+++ forwarder/src/main/scala/com/snowplowanalytics/snowplow/storage/bigquery/forwarder/CommandLine.scala
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2018 Snowplow Analytics Ltd. All rights reserved.
+ *
+ * This program is licensed to you under the Apache License Version 2.0,
+ * and you may not use this file except in compliance with the Apache License Version 2.0.
+ * You may obtain a copy of the Apache License Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the Apache License Version 2.0 is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
+ */
+package com.snowplowanalytics.snowplow.storage.bigquery
+package forwarder
+
+import cats.syntax.either._
+
+import com.spotify.scio.Args
+
+import common.Config._
+
+/**
+  * Forwarder specific CLI configuration
+  * Unlike Mutator, required --key=value format and ignores unknown options (for Dataflow)
+  * Unlike Loader, also required `--failedInsertsSub`
+  */
+object CommandLine {
+  case class ForwarderEnvironment(common: Environment, failedInserts: String) {
+    def getFullFailedInsertsSub: String = s"projects/${common.config.projectId}/subscriptions/$failedInserts"
+  }
+
+  def parse(args: Args): ForwarderEnvironment = {
+    val environment =
+      for {
+        c <- decodeBase64Json(args("config"))
+        r <- decodeBase64Json(args("resolver"))
+        e <- transform(EnvironmentConfig(r, c))
+        s <- Either.catchNonFatal(args("failedInsertsSub"))
+      } yield ForwarderEnvironment(e, s)
+
+    environment.fold(throw _, identity)
+  }
+}
---
+++ forwarder/src/main/scala/com/snowplowanalytics/snowplow/storage/bigquery/forwarder/Forwarder.scala
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2018 Snowplow Analytics Ltd. All rights reserved.
+ *
+ * This program is licensed to you under the Apache License Version 2.0,
+ * and you may not use this file except in compliance with the Apache License Version 2.0.
+ * You may obtain a copy of the Apache License Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the Apache License Version 2.0 is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
+ */
+package com.snowplowanalytics.snowplow.storage.bigquery
+package forwarder
+
+import com.google.api.services.bigquery.model.TableReference
+
+import org.joda.time.Duration
+
+import com.spotify.scio.ScioContext
+
+import org.apache.beam.sdk.io.gcp.bigquery.BigQueryIO
+import org.apache.beam.sdk.io.gcp.bigquery.BigQueryIO.Write.{CreateDisposition, WriteDisposition}
+
+import com.spotify.scio.bigquery.TableRow
+
+import com.snowplowanalytics.snowplow.storage.bigquery.forwarder.CommandLine.ForwarderEnvironment
+
+object Forwarder {
+  def run(env: ForwarderEnvironment, sc: ScioContext): Unit = {
+    val tableRef = new TableReference()
+      .setProjectId(env.common.config.projectId)
+      .setDatasetId(env.common.config.datasetId)
+      .setTableId(env.common.config.tableId)
+
+    val output: BigQueryIO.Write[TableRow] =
+      BigQueryIO.writeTableRows()
+        .withCreateDisposition(CreateDisposition.CREATE_NEVER)
+        .withWriteDisposition(WriteDisposition.WRITE_APPEND)
+        .withMethod(BigQueryIO.Write.Method.FILE_LOADS)
+        .withNumFileShards(50)
+        .withTriggeringFrequency(Duration.standardSeconds(300))
+        .to(tableRef)
+
+    sc.pubsubSubscription[TableRow](env.getFullFailedInsertsSub).
+      saveAsCustomOutput(env.common.config.tableId, output)
+  }
+}

708ee3e47f5f9522f313cad4f3d546e79bf1fcbf
Dilyan Damyanov <dilyand@gmail.com>
2020-03-06 12:04:24 +0000

    Bump Scala to 2.12.10 (close #58)

--- project/BuildSettings.scala
+++ project/BuildSettings.scala
@@ -24,7 +24,7 @@ object BuildSettings {
   lazy val commonSettings = Seq(
     organization          := "com.snowplowanalytics",
     version               := "0.3.0",
-    scalaVersion          := "2.12.8",
+    scalaVersion          := "2.12.10",
     scalacOptions         ++= Seq("-target:jvm-1.8",
       "-language:existentials",
       "-language:higherKinds",

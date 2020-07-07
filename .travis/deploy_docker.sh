48f8e00fdeb24666ee1412a0ef2e1df2e8d228ce
Dilyan Damyanov <dilyand@gmail.com>
2020-07-07 20:40:09 +0100

    Add fs2loader to docker deployment

--- .travis/deploy_docker.sh
+++ .travis/deploy_docker.sh
@@ -13,11 +13,13 @@ cd $TRAVIS_BUILD_DIR
 project_version=$(sbt "project common" version -Dsbt.log.noformat=true | perl -ne 'print "$1\n" if /info.*(\d+\.\d+\.\d+[^\r\n]*)/' | tail -n 1 | tr -d '\n')
 if [[ "${tag}" = *"${project_version}" ]]; then
     sbt "project loader" docker:publishLocal
+    sbt "project fs2loader" docker:publishLocal
     sbt "project mutator" docker:publishLocal
     sbt "project repeater" docker:publishLocal
     sbt "project forwarder" docker:publishLocal
     docker push "${docker_repo}/snowplow/snowplow-bigquery-mutator:${tag}"
     docker push "${docker_repo}/snowplow/snowplow-bigquery-loader:${tag}"
+    docker push "${docker_repo}/snowplow/snowplow-bigquery-fs2loader:${tag}"
     docker push "${docker_repo}/snowplow/snowplow-bigquery-repeater:${tag}"
     docker push "${docker_repo}/snowplow/snowplow-bigquery-forwarder:${tag}"
 else

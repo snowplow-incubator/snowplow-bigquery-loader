cca8c581fc0bddbe6fd5560a99f6e517447d80cd
Anton Parkhomenko <mailbox@chuwy.me>
2020-11-18 11:05:17 +0300

    [DELETEME] Keep only repeater publishing

--- .github/deploy_docker.sh
+++ .github/deploy_docker.sh
@@ -14,16 +14,8 @@ echo '{"auths": '"$cfg"'}' > $file
 project_version=$(sbt "project common" version -Dsbt.log.noformat=true | perl -ne 'print "$1\n" if /info.*(\d+\.\d+\.\d+[^\r\n]*)/' | tail -n 1 | tr -d '\n')
 if [[ "${tag}" = *"${project_version}" ]]; then
     echo "Building and publishing ${docker_repo}/snowplow/snowplow-bigquery-repeater:${tag} ($project_version version)"
-    sbt "project loader" docker:publishLocal
-    sbt "project streamloader" docker:publishLocal
-    sbt "project mutator" docker:publishLocal
     sbt "project repeater" docker:publishLocal
-    sbt "project forwarder" docker:publishLocal
-    docker push "${docker_repo}/snowplow/snowplow-bigquery-mutator:${tag}"
-    docker push "${docker_repo}/snowplow/snowplow-bigquery-loader:${tag}"
-    docker push "${docker_repo}/snowplow/snowplow-bigquery-streamloader:${tag}"
     docker push "${docker_repo}/snowplow/snowplow-bigquery-repeater:${tag}"
-    docker push "${docker_repo}/snowplow/snowplow-bigquery-forwarder:${tag}"
 else
     echo "Tag version '${tag}' doesn't match version in scala project ('${project_version}'). Aborting!"
     exit 1

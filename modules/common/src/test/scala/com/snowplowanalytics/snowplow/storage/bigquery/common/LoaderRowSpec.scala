8cd36ab9e21e26344c06d89071b523f2164a5045
Dilyan Damyanov <dilyand@gmail.com>
2021-07-08 09:35:41 +0100

    StreamLoader: add metrics (close #165)

---
+++ modules/common/src/test/scala/com/snowplowanalytics/snowplow/storage/bigquery/common/LoaderRowSpec.scala
@@ -0,0 +1,215 @@
+/*
+ * Copyright (c) 2018-2021 Snowplow Analytics Ltd. All rights reserved.
+ *
+ * This program is licensed to you under the Apache License Version 2.0,
+ * and you may not use this file except in compliance with the Apache License Version 2.0.
+ * You may obtain a copy of the Apache License Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the Apache License Version 2.0 is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
+ */
+package com.snowplowanalytics.snowplow.storage.bigquery.common
+
+import com.snowplowanalytics.iglu.client.resolver.Resolver
+import com.snowplowanalytics.iglu.core.SchemaKey
+import com.snowplowanalytics.iglu.core.SchemaVer.Full
+import com.snowplowanalytics.iglu.schemaddl.bigquery.Row.{Primitive, Record, Repeated}
+import com.snowplowanalytics.snowplow.badrows.Processor
+import com.snowplowanalytics.snowplow.storage.bigquery.common.Adapter._
+import com.snowplowanalytics.snowplow.storage.bigquery.common.LoaderRow.transformJson
+import com.snowplowanalytics.snowplow.storage.bigquery.common.SpecHelpers.implicits.idClock
+
+import cats.Id
+import com.google.api.services.bigquery.model.TableRow
+import io.circe.literal._
+import io.circe.parser.parse
+import org.joda.time.Instant
+import org.specs2.mutable.Specification
+
+class LoaderRowSpec extends Specification {
+  val processor: Processor   = SpecHelpers.meta.processor
+  val resolver: Resolver[Id] = SpecHelpers.iglu.resolver
+
+  "groupContexts" should {
+    "group contexts with same version" in {
+      val contexts = SpecHelpers.events.geoContexts
+
+      val result = LoaderRow
+        .groupContexts(resolver, contexts)
+        .toEither
+        .map(x => x.map { case (k, v) => (k, v.asInstanceOf[java.util.List[String]].size()) }.toMap)
+
+      result must beRight(
+        Map(
+          "contexts_com_snowplowanalytics_snowplow_geolocation_context_1_0_0" -> 2,
+          "contexts_com_snowplowanalytics_snowplow_geolocation_context_1_1_0" -> 1
+        )
+      )
+    }
+  }
+
+  "fromEvent" should {
+    "transform all JSON types into their AnyRef counterparts" in {
+      // INPUTS
+      val event           = SpecHelpers.events.exampleEvent
+      val contexts        = SpecHelpers.events.contexts
+      val derivedContexts = SpecHelpers.events.derivedContexts
+      val unstructEvent   = SpecHelpers.events.unstructEvent
+
+      val inputPlain = event.copy(
+        br_cookies        = Some(false),
+        domain_sessionidx = Some(3)
+      )
+      val inputC = event.copy(
+        contexts          = contexts,
+        br_cookies        = Some(false),
+        domain_sessionidx = Some(3)
+      )
+      val inputDc = event.copy(
+        br_cookies        = Some(false),
+        domain_sessionidx = Some(3),
+        derived_contexts  = derivedContexts
+      )
+      val inputUe = event.copy(
+        br_cookies        = Some(false),
+        domain_sessionidx = Some(3),
+        unstruct_event    = unstructEvent
+      )
+      val inputUeC = event.copy(
+        contexts          = contexts,
+        br_cookies        = Some(false),
+        domain_sessionidx = Some(3),
+        unstruct_event    = unstructEvent
+      )
+      val inputUeDc = event.copy(
+        br_cookies        = Some(false),
+        domain_sessionidx = Some(3),
+        unstruct_event    = unstructEvent,
+        derived_contexts  = derivedContexts
+      )
+      val inputUeDcC = event.copy(
+        contexts          = contexts,
+        br_cookies        = Some(false),
+        domain_sessionidx = Some(3),
+        unstruct_event    = unstructEvent,
+        derived_contexts  = derivedContexts
+      )
+
+      // OUTPUTS
+      val resultPlain = LoaderRow.fromEvent(resolver, processor)(inputPlain)
+      val resultC     = LoaderRow.fromEvent(resolver, processor)(inputC)
+      val resultDc    = LoaderRow.fromEvent(resolver, processor)(inputDc)
+      val resultUe    = LoaderRow.fromEvent(resolver, processor)(inputUe)
+      val resultUeC   = LoaderRow.fromEvent(resolver, processor)(inputUeC)
+      val resultUeDc  = LoaderRow.fromEvent(resolver, processor)(inputUeDc)
+      val resultUeDcC = LoaderRow.fromEvent(resolver, processor)(inputUeDcC)
+
+      // EXPECTATIONS
+      val cValue  = adaptRow(Repeated(List(Record(List(("id", Primitive("deadbeef-0000-1111-2222-deadbeef3333")))))))
+      val dcValue = adaptRow(Repeated(List(Record(List(("a", Primitive("b")))))))
+      val ueValue = adaptRow(Record(List(("c", Primitive("d")))))
+
+      val tableRowPlain = new TableRow()
+        .set("v_collector", "bq-loader-test")
+        .set("collector_tstamp", "2019-02-18T08:06:07.580Z")
+        .set("br_cookies", false)
+        .set("domain_sessionidx", 3)
+        .set("event_id", "ba553b7f-63d5-47ad-8697-06016b472c34")
+        .set("v_etl", "bq-loader-test")
+      val tableRowC = new TableRow()
+        .set("v_collector", "bq-loader-test")
+        .set("collector_tstamp", "2019-02-18T08:06:07.580Z")
+        .set("br_cookies", false)
+        .set("domain_sessionidx", 3)
+        .set("event_id", "ba553b7f-63d5-47ad-8697-06016b472c34")
+        .set("v_etl", "bq-loader-test")
+        .set("contexts_com_snowplowanalytics_snowplow_web_page_1_0_0", cValue)
+      val tableRowDc = new TableRow()
+        .set("v_collector", "bq-loader-test")
+        .set("collector_tstamp", "2019-02-18T08:06:07.580Z")
+        .set("br_cookies", false)
+        .set("domain_sessionidx", 3)
+        .set("event_id", "ba553b7f-63d5-47ad-8697-06016b472c34")
+        .set("v_etl", "bq-loader-test")
+        .set("contexts_com_snowplowanalytics_snowplow_derived_context_1_0_0", dcValue)
+      val tableRowUe = new TableRow()
+        .set("v_collector", "bq-loader-test")
+        .set("collector_tstamp", "2019-02-18T08:06:07.580Z")
+        .set("br_cookies", false)
+        .set("domain_sessionidx", 3)
+        .set("event_id", "ba553b7f-63d5-47ad-8697-06016b472c34")
+        .set("v_etl", "bq-loader-test")
+        .set("unstruct_event_com_snowplowanalytics_snowplow_unstruct_event_1_0_0", ueValue)
+      val tableRowUeC = new TableRow()
+        .set("v_collector", "bq-loader-test")
+        .set("collector_tstamp", "2019-02-18T08:06:07.580Z")
+        .set("br_cookies", false)
+        .set("domain_sessionidx", 3)
+        .set("event_id", "ba553b7f-63d5-47ad-8697-06016b472c34")
+        .set("v_etl", "bq-loader-test")
+        .set("contexts_com_snowplowanalytics_snowplow_web_page_1_0_0", cValue)
+        .set("unstruct_event_com_snowplowanalytics_snowplow_unstruct_event_1_0_0", ueValue)
+      val tableRowUeDc = new TableRow()
+        .set("v_collector", "bq-loader-test")
+        .set("collector_tstamp", "2019-02-18T08:06:07.580Z")
+        .set("br_cookies", false)
+        .set("domain_sessionidx", 3)
+        .set("event_id", "ba553b7f-63d5-47ad-8697-06016b472c34")
+        .set("v_etl", "bq-loader-test")
+        .set("unstruct_event_com_snowplowanalytics_snowplow_unstruct_event_1_0_0", ueValue)
+        .set("contexts_com_snowplowanalytics_snowplow_derived_context_1_0_0", dcValue)
+      val tableRowUeDcC = new TableRow()
+        .set("v_collector", "bq-loader-test")
+        .set("collector_tstamp", "2019-02-18T08:06:07.580Z")
+        .set("br_cookies", false)
+        .set("domain_sessionidx", 3)
+        .set("event_id", "ba553b7f-63d5-47ad-8697-06016b472c34")
+        .set("v_etl", "bq-loader-test")
+        .set("contexts_com_snowplowanalytics_snowplow_web_page_1_0_0", cValue)
+        .set("unstruct_event_com_snowplowanalytics_snowplow_unstruct_event_1_0_0", ueValue)
+        .set("contexts_com_snowplowanalytics_snowplow_derived_context_1_0_0", dcValue)
+
+      // format: off
+      val tstamp = event.collector_tstamp.toEpochMilli
+      
+      val expectedPlain = LoaderRow(new Instant(tstamp), tableRowPlain, inputPlain.inventory)
+      val expectedC     = LoaderRow(new Instant(tstamp), tableRowC, inputC.inventory)
+      val expectedDc    = LoaderRow(new Instant(tstamp), tableRowDc, inputDc.inventory)
+      val expectedUe    = LoaderRow(new Instant(tstamp), tableRowUe, inputUe.inventory)
+      val expectedUeC   = LoaderRow(new Instant(tstamp), tableRowUeC, inputUeC.inventory)
+      val expectedUeDc  = LoaderRow(new Instant(tstamp), tableRowUeDc, inputUeDc.inventory)
+      val expectedUeDcC = LoaderRow(new Instant(tstamp), tableRowUeDcC, inputUeDcC.inventory)
+      // format: on
+
+      // OUTCOMES
+      resultPlain must beRight(expectedPlain)
+      resultC must beRight(expectedC)
+      resultDc must beRight(expectedDc)
+      resultUe must beRight(expectedUe)
+      resultUeC must beRight(expectedUeC)
+      resultUeDc must beRight(expectedUeDc)
+      resultUeDcC must beRight(expectedUeDcC)
+    }
+  }
+
+  "transformJson" should {
+    "succeed with an event whose schema has an [array, null] property" in {
+      val json = parse(SpecHelpers.events.nullableArrayUnstructData).getOrElse(json"""{"n": "a"}""")
+      val result =
+        transformJson(
+          resolver,
+          SchemaKey("com.snowplowanalytics.snowplow", "nullable_array_event", "jsonschema", Full(1, 0, 0))
+        )(json).toEither
+
+      result must beRight
+    }
+  }
+
+  "parse" should {
+    "succeed with an event whose schema has an [array, null] property" in {
+      LoaderRow.parse(resolver, processor)(SpecHelpers.events.nullableArrayUnstructEvent) must beRight
+    }
+  }
+}
---
+++ modules/repeater/src/test/scala/com/snowplowanalytics/snowplow/storage/bigquery/repeater/RepeaterCliSpec.scala
@@ -0,0 +1,114 @@
+/*
+ * Copyright (c) 2018-2021 Snowplow Analytics Ltd. All rights reserved.
+ *
+ * This program is licensed to you under the Apache License Version 2.0,
+ * and you may not use this file except in compliance with the Apache License Version 2.0.
+ * You may obtain a copy of the Apache License Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the Apache License Version 2.0 is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
+ */
+package com.snowplowanalytics.snowplow.storage.bigquery.repeater
+
+import com.snowplowanalytics.snowplow.storage.bigquery.common.SpecHelpers
+import com.snowplowanalytics.snowplow.storage.bigquery.repeater.RepeaterCli.{GcsPath, ListenCommand, validateBucket}
+
+import cats.data.NonEmptyList
+import org.specs2.mutable.Specification
+
+class RepeaterCliSpec extends Specification {
+  "parse" should {
+    "extract valid Repeater configuration" in {
+      val expected = ListenCommand(SpecHelpers.configs.repeaterEnv, 20, 30, 900, false)
+      val result =
+        RepeaterCli.parse(
+          Seq(
+            "--config",
+            SpecHelpers.configs.validHoconB64,
+            "--resolver",
+            SpecHelpers.configs.validResolverJsonB64,
+            "--bufferSize",
+            "20",
+            "--timeout",
+            "30",
+            "--backoffPeriod",
+            "900"
+          )
+        )
+
+      result must beRight(expected)
+    }
+
+    "fail to extract valid configuration" in {
+      val invalidHoconRes = RepeaterCli.parse(
+        Seq(
+          "--config",
+          SpecHelpers.configs.invalidHoconB64,
+          "--resolver",
+          SpecHelpers.configs.validResolverJsonB64,
+          "--bufferSize",
+          "20",
+          "--timeout",
+          "30",
+          "--backoffPeriod",
+          "900"
+        )
+      )
+      val malformedJsonRes = RepeaterCli.parse(
+        Seq(
+          "--config",
+          SpecHelpers.configs.validHoconB64,
+          "--resolver",
+          SpecHelpers.configs.malformedResolverJsonB64,
+          "--bufferSize",
+          "20",
+          "--timeout",
+          "30",
+          "--backoffPeriod",
+          "900"
+        )
+      )
+      val invalidJsonRes = RepeaterCli.parse(
+        Seq(
+          "--config",
+          SpecHelpers.configs.validHoconB64,
+          "--resolver",
+          SpecHelpers.configs.invalidResolverJsonB64,
+          "--bufferSize",
+          "20",
+          "--timeout",
+          "30",
+          "--backoffPeriod",
+          "900"
+        )
+      )
+
+      List(invalidHoconRes, malformedJsonRes, invalidJsonRes).forall(_ must beLeft)
+    }
+  }
+
+  "validateBucket" should {
+    "succeed with a valid input" in {
+      val input    = "gs://my-bucket/my-folder"
+      val expected = GcsPath("my-bucket", "my-folder/")
+
+      validateBucket(input).toEither must beRight(expected)
+    }
+
+    "fail if the 'gs://' prefix is missing" in {
+      val input    = "my-bucket/my-folder"
+      val expected = NonEmptyList.one("GCS bucket must start with gs://")
+
+      validateBucket(input).toEither must beLeft(expected)
+    }
+
+    "fail if only the 'gs://' protocol is specified" in {
+      val input    = "gs://"
+      val expected = NonEmptyList.one("GCS bucket cannot be empty")
+
+      validateBucket(input).toEither must beLeft(expected)
+    }
+  }
+}

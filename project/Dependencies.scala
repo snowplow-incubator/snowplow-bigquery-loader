00be5441cc3279a4698f9bc508a712dc1b764527
Dilyan Damyanov <dilyand@gmail.com>
2022-02-24 16:22:48 +0000

    Bump log4cats-slf4j to 1.5.1 (close #256)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -41,7 +41,7 @@ object Dependencies {
     val decline     = "1.4.0"
     val fs2         = "2.5.9"
     val httpClient  = "0.22.5"
-    val logging     = "1.4.0"
+    val logging     = "1.5.1"
     val pubsubFs2   = "0.18.1"
     val scio        = "0.11.4"
     val circeConfig = "0.8.0"

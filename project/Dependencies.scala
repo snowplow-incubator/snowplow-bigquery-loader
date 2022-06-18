9caab5878eb4dfe5d0abc9bdd9e1370763491ed5
Ian Streeter <ian@snowplowanalytics.com>
2022-06-18 20:58:21 +0100

    Bump google-cloud-bigquery to 2.13.0 (close #291)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -17,7 +17,7 @@ object Dependencies {
   object V {
     // Java
     val beam                = "2.38.0"
-    val googleCloudBigQuery = "2.10.10" // compatible with google-cloud-bom:0.173.0
+    val googleCloudBigQuery = "2.13.0" // compatible with google-cloud-bom:0.175.0
     val googleCloudPubSub   = "1.116.4" // compatible with google-cloud-bom:0.173.0
     val googleCloudStorage  = "2.6.1" // compatible with google-cloud-bom:0.173.0
     object Beam {
